d1c784fde14f4210bfc2a9f48ad7512f0b19d7b0
ElaGolubkova <156133688+ElaGolubkova@users.noreply.github.com>
2024-01-24 21:33:09 -0800

    Session 1 Prompt for Journal 1

---
+++ Introduction.py
@@ -0,0 +1,5 @@
+a = "Hi! My name is Elvira Golubkova, my pronouns are she/her."
+b = ("My favorite food is Pizza! I feel like it is a very universal\n"
+     "dish that many people enjoy and is very easy to prepare :)")
+
+print(f"{a}\n{b}")
