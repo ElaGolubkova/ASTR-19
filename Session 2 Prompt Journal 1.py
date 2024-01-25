7d71117a3c8f1069389d6988a70ef44fc58db82b
ElaGolubkova <156133688+ElaGolubkova@users.noreply.github.com>
2024-01-24 22:12:42 -0800

    Session 2 Prompt Journal 1

---
+++ Session 2 Prompt Journal 1.py
@@ -0,0 +1,23 @@
+# Sum of two floating point numbers
+a = 13.0
+b = 5.0
+sum = a+b
+
+print(sum)
+print("type(sum):", type(sum))
+
+# Difference between two integers
+c = 27
+d = 2
+diff = c-d
+
+print(diff)
+print("type(difference):", type(diff))
+
+# Product of a floating point number and an integer
+e = 37.7
+f = 2
+mult = e*f
+
+print(mult)
+print("type(multiplication):", type(mult))
